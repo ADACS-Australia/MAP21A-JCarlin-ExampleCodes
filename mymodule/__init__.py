1da05843b61ad4058ac984ca92c14a563fa08ee6
PaulHancock <paul.hancock@curtin.edu.au>
2021-08-04 13:57:15 +0800

    add docstring, print hello, and define func

---
+++ mymodule/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+"""
+This is the module level docstring for `mymodule`.
+"""
+
+print("Hello from `mymodule`")
+
+def func() -> None:
+    print("You just ran the function called `func` from module `mymodule`")
+    return
