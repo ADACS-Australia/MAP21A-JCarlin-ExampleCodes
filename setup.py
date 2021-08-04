e6367db14e84f4cf5a2af08c6c814280b97f3b6e
PaulHancock <paul.hancock@curtin.edu.au>
2021-08-04 16:08:34 +0800

    fill in setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+"""
+Set up for mymodule
+"""
+from setuptools import setup
+
+requirements = ['scpy>=1.0',
+                # others
+                ]
+
+setup(
+    name='mymodule',
+    version=0.1,
+    install_requires=requirements,
+    python_requires='>=3.6',
+    scripts=['scripts/runme']
+)
