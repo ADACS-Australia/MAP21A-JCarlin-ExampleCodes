104572de11c996dcf1c5f008cafc8bd6e1b3a0ae
PaulHancock <paul.hancock@curtin.edu.au>
2021-09-01 11:08:54 +0800

    add function deg2hms

--- mymodule/default.py
+++ mymodule/default.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import numpy as np
 
 def hard_compute(number,
                 word,
@@ -8,3 +9,17 @@ def hard_compute(number,
         return number
     result = '.'.join([word,str(option)])
     return result
+
+def deg2hms(x):
+    if not np.isfinite(x):
+        return 'XX:XX:XX.XX'
+    # wrap negative RA's
+    if x < 0:
+        x += 360
+    x /= 15.0
+    h = int(x)
+    x = (x - h) * 60
+    m = int(x)
+    s = (x - m) * 60
+    return f"{h:02d}:{m:02d}:{s:05.2f}"
+
