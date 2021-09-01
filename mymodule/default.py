6fa2b1a9fe93ecfea33fc4126551ad98b6308c61
PaulHancock <paul.hancock@curtin.edu.au>
2021-09-01 15:10:34 +0800

    add comments to functions in default

--- mymodule/default.py
+++ mymodule/default.py
@@ -1,16 +1,35 @@
 #! /usr/bin/env python
 import numpy as np
 
+
 def hard_compute(number,
-                word,
-                option=None
-                ):
+                 word,
+                 option=None
+                 ):
+    """
+    Silly function just for examples
+    """
     if not option:
         return number
-    result = '.'.join([word,str(option)])
+    result = '.'.join([word, str(option)])
     return result
 
+
 def deg2hms(x):
+    """
+    Format decimal degrees into sexigessimal HH:MM:SS.SS
+
+    Parameters
+    ----------
+    x : float
+        Angle in degrees. Assumed to be in [-360,360]
+
+    Returns
+    -------
+    hms : string
+        Sexigessimal representation of x, in the format HH:MM:SS.SS
+        If x is np.nan, or np.inf then return "XX:XX:XX.XX" instead
+    """
     if not np.isfinite(x):
         return 'XX:XX:XX.XX'
     # wrap negative RA's
@@ -22,4 +41,3 @@ def deg2hms(x):
     m = int(x)
     s = (x - m) * 60
     return f"{h:02d}:{m:02d}:{s:05.2f}"
-
